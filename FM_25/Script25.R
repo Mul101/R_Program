239de7aee75f2991d26c54a094b890df710a09d7
Mul101 <mulia4404@gmail.com>
2020-07-04 12:22:00 +0700

    FM_25

---
+++ FM_25/Script25.R
@@ -0,0 +1,6 @@
+#Menggunakan Sintax FMA_Mr
+library(readxl)
+dataFMA <- read_excel("c:/users/lenovo/Documents/FMA_Mr..xlsx")
+View(dataFMA)
+dataFMB <- read_excel("c:/users/lenovo/Documents/FMB_Mr..xlsx")
+View(dataFMB)
